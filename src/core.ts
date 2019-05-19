e8bbdbb0f25168e983ebcdfe46d441fc2eae57b2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-05-20 02:33:14 +0800

    update css url

--- src/core.ts
+++ src/core.ts
@@ -450,7 +450,7 @@ export function minifyHTML(s: string, options?: MinifyHTMLOptions): string {
             '<link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" type="text/css">',
           ),
           ...parseHTMLTree(
-            '<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">',
+            '<link rel="stylesheet" href="https://unpkg.com/@beenotung/sakura.css/css/sakura.css" type="text/css">',
           ),
         );
       });
