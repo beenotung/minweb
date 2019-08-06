9b852ac2fd2b4e48d4ca37202da6c5701aa8e494
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-08-06 21:23:33 +0800

    restored origin to sandbox version

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export let baseUrl = '/minWeb';
 export let origin =
-  'https://wt-2f31e8aca451cf5494a2ee7270b6a7dc-0.run.webtask.io';
+  'https://wt-2f31e8aca451cf5494a2ee7270b6a7dc-0.sandbox.auth0-extend.com';
